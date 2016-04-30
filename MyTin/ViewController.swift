50da62a5af796661d358fb4a7fc60fae2612aa39
LinChengYen <kindping@gmail.com>
2016-04-30 11:07:18 +0800

    Initial Commit

---
+++ MyTin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTin
+//
+//  Created by LinChengYen on 2016/4/30.
+//  Copyright © 2016年 LinChengYen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
